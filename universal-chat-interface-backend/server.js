1e06cd96094e70964f01eddbfe41dfb81738b280
IndependentGames <85473506+IndependentGames@users.noreply.github.com>
2023-05-28 23:36:37 -0600

    v0.1.3

---
+++ universal-chat-interface-backend/server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const port = 3000; // Choose a suitable port number
+
+app.use(express.json());
+
+app.post('/api/send-message', async (req, res) => {
+  const { message } = req.body;
+  console.log('Received request:', message);
+  try {
+    const response = await axios.post(
+        "https://api.openai.com/v1/chat/completions",
+      {
+        messages: [{ role: 'system', content: 'You are a user' }, { role: 'user', content: message }],
+      },
+      {
+        headers: {
+          'Authorization': 'Bearer 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',
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+
+    // Extract the generated chat message from the API response
+    const generatedMessage = response.data.choices[0].message.content;
+
+    // Send the generated chat message back to the frontend
+    res.json({ response: generatedMessage });
+  } catch (error) {
+    console.error('Error:', error);
+    // Handle any error cases here
+    // You can return an error message or handle the error as needed
+    res.status(500).json({ error: 'An error occurred' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
